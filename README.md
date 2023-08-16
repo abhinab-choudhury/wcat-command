2b2bca5a9323b0f3f6beefd265db2964802ebd63
abhinab-choudhury <abhinabchoudhury291@gmail.com>
2023-08-16 23:29:22 +0530

    v2.0.0

--- README.md
+++ README.md
@@ -13,7 +13,6 @@ It is used to display or make a copy content of one or more files in the termina
 1. *wcat filepath* => displays content of the file in the terminal ✔
 2. *wcat filepath1 filepath2 filepath3...* => displays content of all files in the terminal(contactinated form) in the given order. ✔
 3. *wcat -s filepath* => convert big line breaks into a singular line break ✔
-
 4. *wcat -n filepath* => give numbering to all the lines  ✔
 5. *wcat -b filepath* => give numbering to non-empty lines  ✔
 6. *wcat filepath* > filename2path => put all the content of filename into filename2 by overriding and also creates ~filename2~ if it doesn't exist. ✔
---
+++ main.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const AVILABLE_FUNC = ['-n', '-b', '-s' ]
+
+const COMMAND = process.argv.slice(2)
+
+const INPUT_FILE = []
+const FUNC = []
+
+
+for(let i = 0;i < COMMAND.length; i++) { 
+    if(COMMAND[i].charAt(0) === '-') {
+        if(AVILABLE_FUNC.includes(COMMAND[i])) {
+            FUNC.push(COMMAND[i])
+        } else {
+            console.log(`${COMMAND[i].slice(1)} IS NOT AVILABLE`)
+            return
+        }
+    } else {
+        if(fs.existsSync(COMMAND[i])) {
+            INPUT_FILE.push(COMMAND[i])
+        } else {
+            console.log(`FILE \"${COMMAND[i].slice(1)}\" DOESN'T EXISTS`)
+            return
+        }
+    }
+}
+
+// Reading the Text inside the file....
+let TEXT_FILE = ""
+for(let i = 0;i < INPUT_FILE.length; i++) {
+    let temp = fs.readFileSync(INPUT_FILE[i]) + ""
+    TEXT_FILE += temp + '\n' 
+}
+
+// FUNCTION....
+for(let i = 0;i < FUNC.length; i++) {
+    TEXT_FILE = func(TEXT_FILE, FUNC[i])
+}
+console.log(TEXT_FILE)
+
+function func(text, func) {
+    if(func === '-n') {
+        let tempArr = text.split('\n') 
+        text = ""
+        for(let i = 0;i < tempArr.length; i++) {
+            if(i > 0 && i < 10) {
+                text += (i+1) + "  " + tempArr[i] + "\n"
+            } else {
+                text += (i+1) + " " + tempArr[i] + "\n"
+            }
+        }
+    } else if(func === '-b') {
+        let tempArr = text.split('\n') 
+        let counter = 1
+        text = ""
+        for(let i = 0;i < tempArr.length; i++) {
+            if(tempArr[i] != '') {
+                if(counter > 0 && counter < 10) {
+                    text += counter + "  " + tempArr[i] + "\n"
+                } else {
+                    text += counter + " " + tempArr[i] + "\n"
+                }
+                counter++
+            } else {
+               text += '\n'
+            }
+        }
+    } else if(func === '-s') {
+        let tempArr = text.split('\n') 
+        let arr = []
+        for(let i = 1;i < tempArr.length; i++) {
+            if(tempArr[i] == '' && tempArr[i-1] == '') {
+                tempArr[i] = null
+            } else if(tempArr[i] == '' && tempArr[i-1] == null) {
+                tempArr[i] = null
+            } 
+        }
+        for(let i = 0;i < tempArr.length; i++) {
+            if(tempArr[i] != null) {
+                arr.push(tempArr[i])
+            }
+        }
+        text = arr.join('\n')
+    }
+
+    return text 
+}
